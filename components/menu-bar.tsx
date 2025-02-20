b7cbe9894c9d7452cd50ac11ec3e5e53d84abac4
nikmet <metlov.nm@yandex.ru>
2025-02-20 19:26:28 +0300

    Ссылка на панель администратора

--- components/menu-bar.tsx
+++ components/menu-bar.tsx
@@ -10,16 +10,27 @@ export interface IMenuBarProps {
 
 export const MenuBar = ({ pages, className }: IMenuBarProps): React.JSX.Element => {
     return (
-        <div className={cn("menu-bar-animation w-[300px] bg-secondary m-4 rounded-md -translate-x-[110%]", className)}>
-            {pages.map(page => (
-                <Link
-                    key={page.href}
-                    href={page.href}
-                    className="block p-4 dark:text-white text-black hover:bg-primary/5"
-                >
-                    {page.name}
-                </Link>
-            ))}
+        <div
+            className={cn(
+                "menu-bar-animation w-[300px] bg-secondary m-4 rounded-md -translate-x-[110%] flex flex-col align-center justify-between",
+                className
+            )}
+        >
+            <div>
+                {pages.map(page => (
+                    <Link
+                        key={page.href}
+                        href={page.href}
+                        className="block p-4 dark:text-white text-black hover:bg-primary/5"
+                    >
+                        {page.name}
+                    </Link>
+                ))}
+            </div>
+
+            <Link href="/admin" className="p-4 dark:text-white text-black underline">
+                Панель Администратора
+            </Link>
         </div>
     );
 };
