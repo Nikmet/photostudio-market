c4124a1aa3d65e87b478e254a8a4aec0cf2a5f0d
nikmet <metlov.nm@yandex.ru>
2025-04-16 23:19:35 +0300

    Заказ футболок

---
+++ app/(root)/souvenirs/t_shirts/page.tsx
@@ -0,0 +1,68 @@
+import { createProduct, creteProductItem, uploadImage } from "@/app/actions";
+import { FormValuesTShirts } from "@/components/admin-forms/t-shirts-form/schema";
+import { TShirtsClientForm } from "@/components/client-forms/t-shirt-client-form";
+import { PageTitle } from "@/components/page-title";
+import { calcTShirtPrice } from "@/lib/prices";
+import { createUid } from "@/lib/uid";
+import { prisma } from "@/prisma/prisma-client";
+import { PrintingSide, Size } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+export default async function CupsClientPage() {
+    let id;
+
+    const lastId = (
+        await prisma.tShirt.findFirst({
+            orderBy: {
+                id: "desc"
+            }
+        })
+    )?.id;
+
+    if (!lastId) {
+        id = createUid("Ф", "1");
+    } else {
+        id = createUid("Ф", (Number(lastId.split("-")[1]) + 1).toString());
+    }
+
+    const handleSubmit = async (data: FormValuesTShirts, userId: string) => {
+        "use server";
+
+        const t_shirt = await prisma.tShirt.create({
+            data: {
+                id: id,
+                name: data.name,
+                printingSide: data.printingSide as PrintingSide,
+                size: data.size as Size,
+                printing_image: await uploadImage(data.printing_image)
+            }
+        });
+
+        const product = await createProduct(
+            t_shirt.id,
+            t_shirt.name,
+            "Сувениры",
+            await calcTShirtPrice(t_shirt),
+            "cups"
+        );
+
+        if (!product) {
+            throw new Error("Не удалось создать продукт");
+        }
+
+        const cart = creteProductItem(product, userId);
+
+        if (!cart) {
+            throw new Error("Не удалось создать продукт в корзине");
+        }
+
+        redirect("/");
+    };
+
+    return (
+        <div>
+            <PageTitle>{`Новая кружка | ${id}`}</PageTitle>
+            <TShirtsClientForm onSubmit={handleSubmit} id={id} />
+        </div>
+    );
+}
---
+++ components/client-forms/t-shirt-client-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import { AdminSelect } from "@/components/admin-components/admin-select";
+import { printingSides, sizes } from "@/@types/enums";
+import { Button } from "@/components/ui";
+import { ImageInput } from "@/components/inputs/image-input";
+import { useSession } from "next-auth/react";
+import { formSchemaTShirts, FormValuesTShirts } from "../admin-forms/t-shirts-form/schema";
+
+export interface ITShirtsFormProps {
+    className?: string;
+    defaultValues?: FormValuesTShirts;
+    onSubmit: (data: FormValuesTShirts, userId: string) => void;
+    id: string;
+}
+
+export const TShirtsClientForm = ({ onSubmit, defaultValues, id, className }: ITShirtsFormProps): React.JSX.Element => {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+        watch
+    } = useForm<FormValuesTShirts>({
+        resolver: zodResolver(formSchemaTShirts),
+        defaultValues: defaultValues || {
+            name: "",
+            printing_image: undefined
+        }
+    });
+
+    const session = useSession();
+
+    setValue("name", `Футболка | ${id}`);
+
+    const submitAction = (data: FormValuesTShirts) => {
+        try {
+            onSubmit(data, session.data?.user.id);
+            toast.success(`Футболка "${data.name}" добавлена в корзину!`);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <div className={className}>
+            <form onSubmit={handleSubmit(submitAction)} className="flex gap-2">
+                <div className="flex gap-4">
+                    <Controller
+                        name="printing_image"
+                        control={control}
+                        render={({ field }) => (
+                            <ImageInput
+                                {...field}
+                                label="Изображение"
+                                errors={errors}
+                                onChange={file => field.onChange(file)} // Передаем файл в форму
+                            />
+                        )}
+                    />
+                    <div className="flex flex-col gap-4 w-[500px]">
+                        <AdminSelect
+                            name="printing_side"
+                            value={watch("printingSide")}
+                            onChange={value => setValue("printingSide", value)}
+                            label={"Стороны печати"}
+                            items={printingSides}
+                            defaultValue={defaultValues?.printingSide}
+                            errors={errors}
+                            required
+                        />
+                        <AdminSelect
+                            name="size"
+                            label={"Размер"}
+                            value={watch("size")}
+                            onChange={value => setValue("size", value)}
+                            items={sizes}
+                            defaultValue={defaultValues?.size}
+                            errors={errors}
+                            required
+                        />
+                        <Button type="submit">{defaultValues ? "Сохранить" : "Создать"}</Button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
